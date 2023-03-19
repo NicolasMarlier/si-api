7c9d5d8ca28c611250241861f7fa5e8fafcdfa98
Nicolas Marlier <nicolasmarlier@macbook-pro-de-nicolas-2.home>
2023-03-19 12:14:18 +0100

    First commit

---
+++ migrate/20230319-create-table-invaders.js
@@ -0,0 +1,35 @@
+
+const up = (db) => {
+    return db.query(
+        `
+        CREATE TABLE invaders
+        (
+            space_id INT NOT NULL,
+            image VARCHAR(255),
+            point INT,
+            city_id INT NOT NULL,
+            city_name VARCHAR(255),
+            name VARCHAR(255),
+            date_pos VARCHAR(100),
+            date_flash VARCHAR(100),
+            position JSONB,
+            user_id INT NOT NULL
+        );
+        CREATE INDEX user_id_idx ON invaders (user_id);
+        CREATE UNIQUE INDEX space_id_user_id_idx ON invaders (space_id, user_id);
+        `
+    )
+}
+
+const down = (db) => {
+    return db.query(
+        `
+        DROP TABLE invaders;
+        `
+    )
+}
+
+module.exports = {
+    up,
+    down
+}
