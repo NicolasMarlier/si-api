3cc74993755663ba152128ffa985a758012266c1
Nicolas Marlier <nmarlier@gmail.com>
2023-10-15 10:54:13 +0200

    WIP hints

---
+++ migrate/20231015-create-hints.js
@@ -0,0 +1,29 @@
+
+const up = (db) => {
+    console.log("Creating table 'hints'...")
+    return db.query(
+        `
+        CREATE TABLE hints
+        (
+            description VARCHAR(255),
+            placed_at TIMESTAMP,
+            position JSONB,
+            user_id INT NOT NULL
+        );
+        CREATE INDEX hint_user_id_idx ON hints (user_id);
+        `
+    )
+}
+
+const down = (db) => {
+    return db.query(
+        `
+        DROP TABLE hints
+        `
+    )
+}
+
+module.exports = {
+    up,
+    down
+}
